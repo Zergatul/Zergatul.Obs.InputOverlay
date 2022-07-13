b14fe295effe7ec2b642e47876ccb655586bec74
Igor Budzhak <z3rgatul@gmail.com>
2022-07-13 17:23:09 +0300

    fixed cross-origin security issue

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net;
 using Zergatul.Obs.InputOverlay.RawInput;
 using Zergatul.Obs.InputOverlay.RawInput.Device;
 using Zergatul.Obs.InputOverlay.XInput;
@@ -26,6 +30,8 @@ namespace Zergatul.Obs.InputOverlay
 
         public void Configure(IApplicationBuilder app, IHostApplicationLifetime hostAppLifetime, IWebSocketHandler handler)
         {
+            string[] addresses = app.ServerFeatures.Get<IServerAddressesFeature>().Addresses.ToArray();
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseWebSockets();
@@ -39,9 +45,17 @@ namespace Zergatul.Obs.InputOverlay
             {
                 if (context.Request.Path == "/ws" && context.WebSockets.IsWebSocketRequest)
                 {
-                    using (var ws = await context.WebSockets.AcceptWebSocketAsync())
+                    if (addresses.Any(a1 => context.Request.Headers.Origin.Any(a2 => OriginMatch(a1, a2))))
                     {
-                        await handler.HandleWebSocket(ws);
+                        using (var ws = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await handler.HandleWebSocket(ws);
+                        }
+                    }
+                    else
+                    {
+                        // deny requests from other origins
+                        context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
                     }
                 }
                 else
@@ -49,6 +63,13 @@ namespace Zergatul.Obs.InputOverlay
                     await next();
                 }
             });
+        }
+
+        private bool OriginMatch(string origin1, string origin2)
+        {
+            Uri uri1 = new Uri(origin1);
+            Uri uri2 = new Uri(origin2);
+            return uri1.Scheme == uri2.Scheme && string.Equals(uri1.Host, uri2.Host, StringComparison.OrdinalIgnoreCase) && uri1.Port == uri2.Port;
         }
     }
 }
